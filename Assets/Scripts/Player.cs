466f5e072feddbfcd898c6a79f841724e98506b7
bfelch <kagrenac@live.com>
2015-05-07 14:37:40 -0500

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	private float boatIntegrity = 1.0f;
+	private int numMonkeys = 10;
+	private int numClicks = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void BoatDamage(float damageTaken) {
+		boatIntegrity -= damageTaken;
+	}
+	
+	public void LoseMonkey(int monkeysLost) {
+		numMonkeys -= monkeysLost;
+	}
+	
+	public void DoClick(float posX, float posY) {
+		numClicks++;
+	}
+}
